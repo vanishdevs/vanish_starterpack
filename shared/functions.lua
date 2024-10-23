92d089dfeb8d6089f0e212d3c102f656e8724ad2
vanishdevs <22zVanish@gmail.com>
2024-10-23 18:11:42 -0400

    chore(shared): Create a shared folder for config & functions

---
+++ shared/functions.lua
@@ -0,0 +1,8 @@
+lib.locale()
+Config = lib.load('shared.config')
+
+-- @param msg (string) The necessary message that will be displayed in the notification
+-- @param source (number) The necessary source if you are sending a notification from the server
+function ShowNotification(msg, source) 
+    return source and lib.notify(source, {description = msg, position = 'top-right', type = 'inform'}) or lib.notify({description = msg, position = 'top-right', type = 'inform'})
+end
