02fe7082fba59ae3ba14c4cef5ea2b616c68d9e5
vanishdevs <22zVanish@gmail.com>
2024-10-23 18:02:54 -0400

    refactor(server): Almost complete re-write (secure functionality)

---
+++ server.lua
@@ -0,0 +1,29 @@
+lib.callback.register('vanishdev:server:CheckIfClaimed', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local identifier = xPlayer.identifier
+    local result = MySQL.scalar.await('SELECT getStarter FROM users WHERE identifier = ?', { identifier })
+    
+    if result then
+        return result == 1
+    end
+    
+    return false
+end)
+
+RegisterNetEvent('vanishdev:server:ClaimStarterPack', function()
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local identifier = xPlayer.identifier
+    local playerPed = GetPlayerPed(source)
+    local playerCoords = GetEntityCoords(playerPed)
+
+    if xPlayer and #(playerCoords - Config.Coords) < 5.0 then
+        for k, v in pairs(Config.Items) do 
+            xPlayer.addInventoryItem(k, v)
+        end
+
+        MySQL.update('UPDATE users SET getStarter = ? WHERE identifier = ?', { 1, identifier })
+
+        ShowNotification(locale('recieved'))
+    end
+end)
