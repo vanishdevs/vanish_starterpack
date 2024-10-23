61feea4fe7ecff82d8fae88bb1fae6482670ac9e
vanishdevs <22zVanish@gmail.com>
2024-10-23 18:13:35 -0400

    refactor(fxmanifest): Define file locations properly & re-format

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,17 +3,7 @@ game 'gta5'
 author 'vanishdev'
 lua54 'yes'
 
-shared_scripts {
-  '@ox_lib/init.lua',
-  '@es_extended/imports.lua',
-  'config.lua'
-}
+shared_scripts { '@ox_lib/init.lua', '@es_extended/imports.lua', 'shared/*.lua' }
 client_script 'client.lua'
-server_scripts {
-  '@oxmysql/lib/MySQL.lua',
-  'server/*.lua'
-}
-
-files {
-  'locales/*.json'
-}
+server_scripts { '@oxmysql/lib/MySQL.lua', 'server.lua }
+files { 'locales/*.json' }
