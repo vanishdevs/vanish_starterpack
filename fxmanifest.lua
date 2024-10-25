84356e476fad5ce080161ffe70eaf39228950ed4
vanishdevs <22zVanish@gmail.com>
2024-10-25 10:20:55 -0400

    fix(fxmanifest): Unfinished string

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,5 @@ version '1.0.0'
 
 shared_scripts { '@ox_lib/init.lua', '@es_extended/imports.lua', 'shared/*.lua' }
 client_script 'client.lua'
-server_scripts { '@oxmysql/lib/MySQL.lua', 'server.lua }
+server_scripts { '@oxmysql/lib/MySQL.lua', 'server.lua' }
 files { 'locales/*.json' }
