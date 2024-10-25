f04df2ba8fe391754ae2cc9e5a86abe97ff900f1
vanishdevs <22zVanish@gmail.com>
2024-10-25 17:02:49 -0400

    ci: Update install.sql for installation for other frameworks

--- install.sql
+++ install.sql
@@ -1,1 +1,8 @@
+-- ESX
 ALTER TABLE `users` ADD `getStarter` INT NOT NULL DEFAULT '0'
+
+-- OX Core
+ALTER TABLE `characters` ADD `getStarter` INT NOT NULL DEFAULT '0'
+
+-- -- QBCore & QBX
+ALTER TABLE `players` ADD `getStarter` INT NOT NULL DEFAULT '0'
